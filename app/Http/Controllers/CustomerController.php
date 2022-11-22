5d7c607f9d566ed08ecdc0b84b25031c2d7fda69
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-22 17:21:34 +0100

    Created customer route and controller.

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'phone_number' => 'required',
+            'address_1' => 'required',
+            'address_2' => 'nullable',
+            'state' => 'required',
+            'local_government' => 'required'
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $user = User::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password'])
+            ]);
+
+            $customer = Customer::create([
+                'phone_number' => $validated['phone_number'],
+                'address_1' => $validated['address_1'],
+                'address_2' => $validated['address_2'],
+                'state' => $validated['state'],
+                'local_government' => $validated['local_government'],
+                'user_id' => $user->id
+            ]);
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            return response([
+                'status' => 'Error',
+                'message' => "Something went wrong! " . $e->getMessage()
+            ], 500);
+        }
+
+        return response([
+            'status' => 'Ok',
+            'message' => 'Customer registered successfully'
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
