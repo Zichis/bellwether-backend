9d22f17beaa1dbbc31cc24f0d5b0cbbc7fb7fd5d
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-22 17:21:10 +0100

    Created customer model and migration.

---
+++ app/Models/Customer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'phone_number',
+        'address_1',
+        'address_2',
+        'state',
+        'local_government',
+        'user_id'
+    ];
+}
