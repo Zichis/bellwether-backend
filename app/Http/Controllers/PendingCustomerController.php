fa1abdf61c13f30333c1b9d6fa4e47162e50406c
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-18 00:32:55 +0100

    Added approve method.

--- app/Http/Controllers/PendingCustomerController.php
+++ app/Http/Controllers/PendingCustomerController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\PendingCustomerResource;
+use App\Models\Customer;
 use App\Models\PendingCustomer;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class PendingCustomerController extends Controller
 {
@@ -61,5 +66,39 @@ class PendingCustomerController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function approve($id)
+    {
+        $pendingCustomer = PendingCustomer::findOrFail($id);
+
+        DB::beginTransaction();
+        try {
+            $nameArray = explode(" ", $pendingCustomer->name);
+            $password = end($nameArray);
+            $user = User::create([
+                'name' => $pendingCustomer->name,
+                'email' => $pendingCustomer->email,
+                'password' => Hash::make($password),
+            ]);
+
+            Customer::create([
+                'phone_number' => $pendingCustomer->phone,
+                'address_1' => $pendingCustomer->address_1,
+                'address_2' => $pendingCustomer->address_2,
+                'state' => $pendingCustomer->state,
+                'local_government' => $pendingCustomer->local_government,
+                'user_id' => $user->id
+            ]);
+            $pendingCustomer->delete();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response([
+                'status' => 'Error',
+                'message' => "Something went wrong! " . $e->getMessage(),
+                'data' => $pendingCustomer,
+            ], 500);
+        }
     }
 }
